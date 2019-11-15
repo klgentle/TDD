8051209c070cad467aecf335c0420c53f559b954
klgentle <klgentle@sina.com>
2019-11-15 09:00:06 +0800

    update

---
+++ GildedRose-Refactoring-Kata/Processing/gilded_rose_safety_net.py
@@ -0,0 +1,14 @@
+#gilded_rose_safety_net
+from text_test_fixture import TextTestFixture 
+
+def test_result():
+    # 运行测试
+    obj = TextTestFixture()
+    obj.test_gilded_rose_update_qulity()
+
+    # compare result
+    with open("original_result.txt","r") as orig_result: 
+        assert(orig_result.read() == obj.read_file_content())
+
+if __name__ == "__main__":
+    test_result()
--- GildedRose-Refactoring-Kata/Processing/text_test_fixture.py
+++ GildedRose-Refactoring-Kata/Processing/text_test_fixture.py
@@ -3,42 +3,46 @@ from __future__ import print_function
 import sys
 from gilded_rose import *
 
-filename = "print_file.txt"
-f = open(filename,"w")
-def printf(*args, file=f):
-    return print(*args,file=f)
 
-def print_file_content(filename=filename):
-    with open(filename,"r") as f:
-        print(f.read())
+class TextTestFixture(object):
+    def __init__(self):
+        self.filename = "print_file.txt"
+        self.f = open(self.filename,"w")
+        self.items = [
+                 Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+                 Item(name="Aged Brie", sell_in=2, quality=0),
+                 Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+                 Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+                 Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+                 Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+                 Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+                 Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+                 Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+                ]
 
-def test_gilded_rose_update_qulity():
-    printf("OMGHAI!")
-    items = [
-             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
-             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
-            ]
-
-    days = 2
-    if len(sys.argv) > 1:
-        days = int(sys.argv[1]) + 1
-    for day in range(days):
-        printf("-------- day %s --------" % day)
-        printf("name, sellIn, quality")
-        for item in items:
-            printf(item)
-        printf("")
-        GildedRose(items).update_quality()
+    def printf(self, *args):
+        return print(*args,file=self.f)
+    
+    def read_file_content(self):
+        with open(self.filename,"r") as f:
+            return f.read()
+    
+    def test_gilded_rose_update_qulity(self):
+        self.printf("OMGHAI!")
+        days = 2
+        if len(sys.argv) > 1:
+            days = int(sys.argv[1]) + 1
+        for day in range(days):
+            self.printf("-------- day %s --------" % day)
+            self.printf("name, sellIn, quality")
+            for item in self.items:
+                self.printf(item)
+            self.printf("")
+            GildedRose(self.items).update_quality()
 
 
 if __name__ == "__main__":
-    test_gilded_rose_update_qulity()
+    obj = TextTestFixture()
+    obj.test_gilded_rose_update_qulity()
     # real print
-    #print_file_content(filename)
+    #print(read_file_content())
